83dd592ea7c5ac5a895292e3f45f1b6bf563bde3
Ayodele Fagbami <afagbami@gmail.com>
2023-04-14 09:25:14 +0100

    created firebase repository

---
+++ lib/features/auth/domain/repositories/firebase_repository.dart
@@ -0,0 +1,19 @@
+abstract class FirebaseRepository {
+  Future<void> signUp(String email, String password);
+  Future<void> signIn(String email, String password);
+  Future<bool> isSignIn();
+  Future<String> getCurrentUid();
+  Future<void> createCurrentUser(
+    String name,
+    String email,
+    String phoneNumber,
+    String address,
+    String pfpURL,
+  );
+  Future<void> signOut();
+  Future<void> googleSignIn();
+  Future<void> googleSignUp();
+  Future<void> setPhone(String phoneNumber);
+  Future<void> setServiceClass(String serviceClass);
+  Future<void> resetPassword(String email);
+}
