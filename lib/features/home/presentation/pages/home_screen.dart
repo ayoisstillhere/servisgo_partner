8d680e68112fbcc0c257a8d319159cb38594a10d
Ayodele Fagbami <afagbami@gmail.com>
2023-05-09 17:40:36 +0100

    fixed home UI

--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -60,71 +60,87 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       key: _scaffoldKey,
       drawer: Drawer(
-        width: getProportionateScreenWidth(260),
-        child: SideMenu(
-          imgUrl: partner.partnerPfpURL,
-          name: partner.partnerName,
-          email: partner.partnerEmail,
-        )
-      ),
+          width: getProportionateScreenWidth(260),
+          child: SideMenu(
+            imgUrl: partner.partnerPfpURL,
+            name: partner.partnerName,
+            email: partner.partnerEmail,
+          )),
       // body: OfflineHome(scaffoldKey: _scaffoldKey, primaryColor: primaryColor),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding:
-              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(32)),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: getProportionateScreenHeight(58)),
-              HamburgerMenuButton(
+      body: Padding(
+        padding:
+            EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(32)),
+        child: Stack(
+          children: [
+            Positioned(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    SizedBox(height: getProportionateScreenHeight(114)),
+                    Center(
+                      child: Text(
+                        "JOB REQUESTS",
+                        style: Theme.of(context)
+                            .textTheme
+                            .displayLarge!
+                            .copyWith(color: primaryColor),
+                      ),
+                    ),
+                    SizedBox(height: getProportionateScreenHeight(20)),
+                    JobRequestCard(
+                      primaryColor: primaryColor,
+                      pfpURL:
+                          "https://firebasestorage.googleapis.com/v0/b/servisgo-fyp.appspot.com/o/Default_PFP.png?alt=media&token=c6cec350-3a9b-4c85-a219-a9d5a8a1a3db",
+                      name: "Ayodele Fagbami",
+                      date: "Sep 15",
+                      time: "14:00",
+                      eta: 15,
+                      service: "Cleaning",
+                      price: 2000,
+                      address: "14C Ola Crescent, High Gardens Estate, Ikota",
+                      city: "Lekki",
+                    ),
+                    SizedBox(height: getProportionateScreenHeight(60)),
+                    JobRequestCard(
+                      primaryColor: primaryColor,
+                      pfpURL:
+                          "https://firebasestorage.googleapis.com/v0/b/servisgo-fyp.appspot.com/o/Default_PFP.png?alt=media&token=c6cec350-3a9b-4c85-a219-a9d5a8a1a3db",
+                      name: "Ugochi Nkem",
+                      date: "Sep 13",
+                      time: "11:00",
+                      eta: 15,
+                      service: "Cleaning",
+                      price: 1000,
+                      address: "8B Ododo Rd, Surulere",
+                      city: "Surulere",
+                    ),
+                    SizedBox(height: getProportionateScreenHeight(60)),
+                    JobRequestCard(
+                      primaryColor: primaryColor,
+                      pfpURL:
+                          "https://firebasestorage.googleapis.com/v0/b/servisgo-fyp.appspot.com/o/Default_PFP.png?alt=media&token=c6cec350-3a9b-4c85-a219-a9d5a8a1a3db",
+                      name: "Ugochi Nkem",
+                      date: "Sep 13",
+                      time: "11:00",
+                      eta: 15,
+                      service: "Cleaning",
+                      price: 1000,
+                      address: "8B Ododo Rd, Surulere",
+                      city: "Surulere",
+                    ),
+                    SizedBox(height: getProportionateScreenHeight(60)),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 58,
+              child: HamburgerMenuButton(
                 scaffoldKey: _scaffoldKey,
                 primaryColor: primaryColor,
               ),
-              Center(
-                child: Text(
-                  "JOB REQUESTS",
-                  style: Theme.of(context)
-                      .textTheme
-                      .displayLarge!
-                      .copyWith(color: primaryColor),
-                ),
-              ),
-              SizedBox(height: getProportionateScreenHeight(20)),
-              JobRequestCard(
-                primaryColor: primaryColor,
-                pfpURL:
-                    "https://firebasestorage.googleapis.com/v0/b/servisgo-fyp.appspot.com/o/Default_PFP.png?alt=media&token=c6cec350-3a9b-4c85-a219-a9d5a8a1a3db",
-                name: "Ayodele Fagbami",
-                date: "Sep 15",
-                time: "14:00",
-                eta: 15,
-                service: "Cleaning",
-                price: 2000,
-                address: "14C Ola Crescent, High Gardens Estate, Ikota",
-                city: "Lekki",
-              ),
-              SizedBox(height: getProportionateScreenHeight(60)),
-              JobRequestCard(
-                primaryColor: primaryColor,
-                pfpURL:
-                    "https://firebasestorage.googleapis.com/v0/b/servisgo-fyp.appspot.com/o/Default_PFP.png?alt=media&token=c6cec350-3a9b-4c85-a219-a9d5a8a1a3db",
-                name: "Ugochi Nkem",
-                date: "Sep 13",
-                time: "11:00",
-                eta: 15,
-                service: "Cleaning",
-                price: 1000,
-                address: "8B Ododo Rd, Surulere",
-                city: "Surulere",
-              ),
-              SizedBox(height: getProportionateScreenHeight(60)),
-              DefaultButton(
-                text: "Go Offline",
-                press: () {},
-              ),
-              SizedBox(height: getProportionateScreenHeight(60)),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
